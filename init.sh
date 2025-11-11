6e3db0a1705e4be9c6bff37f03e2d7bf6576c470
Ignacio Gomez <igomez9@dons.usfca.edu>
2025-11-10 21:43:40 -0800

    add zip files and preprocess script

--- init.sh
+++ init.sh
@@ -19,7 +19,7 @@ unzip ./data/raw/2020q2.zip -d ./data/processed/q2/
 unzip ./data/raw/2020q3.zip -d ./data/processed/q3/
 unzip ./data/raw/2020q4.zip -d ./data/processed/q4/
 
-# for each quarter, convert TSV to CSV
+# for each quarter, convert TSV to CSV and append quarter and year columns
 for quarter in q1 q2 q3 q4; do
     cd ./data/processed/${quarter}/
     for file in ./*.txt; do
@@ -29,17 +29,5 @@ for quarter in q1 q2 q3 q4; do
 done
 
 
-# merge all num.csv files into one, exclude the header rows if not the first file
-mkdir -p ./data/processed/merged
-output_file=./data/processed/merged/num_2020.csv
-first_file=true
-for quarter in q1; do
-    for file in ./data/processed/${quarter}/num.csv; do
-        if [ "$first_file" = true ]; then
-            cat "${file}" >> "${output_file}"
-            first_file=false
-        else
-            tail -n +2 "${file}" >> "${output_file}"
-        fi
-    done
-done
+# run preprocessdata.py
+python3 ./preprocessdata.py
---
+++ preprocessdata.py
@@ -0,0 +1,40 @@
+import pandas as pd
+
+print("Starting data preprocessing...")
+num_files = [
+    "./data/processed/q1/num.csv",
+    "./data/processed/q1/pre.csv",
+    "./data/processed/q1/sub.csv",
+    "./data/processed/q1/tag.csv",
+    "./data/processed/q2/num.csv",
+    "./data/processed/q2/pre.csv",
+    "./data/processed/q2/sub.csv",
+    "./data/processed/q2/tag.csv",
+    "./data/processed/q3/num.csv",
+    "./data/processed/q3/pre.csv",
+    "./data/processed/q3/sub.csv",
+    "./data/processed/q3/tag.csv",
+    "./data/processed/q4/num.csv",
+    "./data/processed/q4/pre.csv",
+    "./data/processed/q4/sub.csv",
+    "./data/processed/q4/tag.csv",
+]
+
+
+for file in num_files:
+    df = pd.read_csv(file)
+    quarter = file.split("/")[3]
+    df["quarter"] = quarter
+    df["year"] = 2020
+    df.to_csv(file, index=False)
+
+
+# merge q1, q2, q3, q4 files into single files
+for prefix in ["num", "pre", "sub", "tag"]:
+    all_dfs = []
+    for quarter in ["q1", "q2", "q3", "q4"]:
+        file = f"./data/processed/{quarter}/{prefix}.csv"
+        df = pd.read_csv(file)
+        all_dfs.append(df)
+    merged_df = pd.concat(all_dfs, ignore_index=True)
+    merged_df.to_csv(f"./data/processed/merged/{prefix}_2020.csv", index=False)
