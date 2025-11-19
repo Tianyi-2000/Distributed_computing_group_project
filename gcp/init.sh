6fbcfe471b7c8abf08138ede49e6649670798dd7
Ignacio Gomez <igomez9@dons.usfca.edu>
2025-11-19 11:56:58 -0800

    Add IAM role creation and user invitations in GCP initialization script

--- gcp/init.sh
+++ gcp/init.sh
@@ -9,3 +9,21 @@ gcloud storage cp ../data/processed/merged/num_2020.csv gs://msds-694-cohort-14-
 gcloud storage cp ../data/processed/merged/pre_2020.csv gs://msds-694-cohort-14-3/data/pre_2020.csv
 gcloud storage cp ../data/processed/merged/sub_2020.csv gs://msds-694-cohort-14-3/data/sub_2020.csv
 gcloud storage cp ../data/processed/merged/tag_2020.csv gs://msds-694-cohort-14-3/data/tag_2020.csv
+
+# create role groupproject with bucket object reader writer and dataproc admin
+gcloud iam roles create groupproject --project=distributed-computing-478219 --title="Group Project Power Users"
+
+# add permissions to role
+gcloud iam roles update groupproject --project=distributed-computing-478219 --add-permissions="storage.objects.get,storage.objects.list,storage.objects.create,storage.objects.delete,dataproc.clusters.create,dataproc.clusters.delete,dataproc.clusters.get,dataproc.clusters.update,dataproc.jobs.create,dataproc.jobs.get,dataproc.jobs.list"
+
+
+# invite users to project
+gcloud projects add-iam-policy-binding distributed-computing-478219 --member="user:igomeza5@gmail.com" --role="projects/distributed-computing-478219/roles/groupproject"
+# chrisjd409@gmail.com
+gcloud projects add-iam-policy-binding distributed-computing-478219 --member="user:chrisjd409@gmail.com" --role="projects/distributed-computing-478219/roles/groupproject"
+# paul.nsn@gmail.com
+gcloud projects add-iam-policy-binding distributed-computing-478219 --member="user:paul.nsn@gmail.com" --role="projects/distributed-computing-478219/roles/groupproject"
+# bostonabautista@gmail.com
+gcloud projects add-iam-policy-binding distributed-computing-478219 --member="user:bostonabautista@gmail.com" --role="projects/distributed-computing-478219/roles/groupproject"
+# alexluous@gmail.com
+gcloud projects add-iam-policy-binding distributed-computing-478219 --member="user:alexluous@gmail.com" --role="projects/distributed-computing-478219/roles/groupproject"
